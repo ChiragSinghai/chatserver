94aa2292485957d8321f22f32070c00c5ae9069e
ChiragSinghai <jainchirag361@gmail.com>
2022-07-18 18:20:10 +0530

    Update asgi.py

--- chatserver/asgi.py
+++ chatserver/asgi.py
@@ -8,21 +8,35 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
+
 import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
+from django.core.handlers.asgi import ASGIHandler
+
+
+def get_asgi_application():
+    """
+    The public interface to Django's ASGI support. Return an ASGI 3 callable.
+
+    Avoids making django.core.handlers.ASGIHandler a public API, in case the
+    internal implementation changes or moves in the future.
+    """
+    django.setup(set_prefix=False)
+    return ASGIHandler()
+
+
+# from django.core.asgi import get_asgi_application
 
 import notify.routing
 import public_chatroom.routing
 import privatechat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatserver.settings")
-django.setup()
 
 application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
-  "websocket": AuthMiddlewareStack(
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
         URLRouter(
             public_chatroom.routing.websocket_urlpatterns +
             privatechat.routing.websocket_urlpatterns +
