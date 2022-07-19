d51f559d11759465b32144342120c4270c4b5a92
ChiragSinghai <jainchirag361@gmail.com>
2022-07-19 11:25:52 +0530

    Update settings.py

--- chatserver/settings.py
+++ chatserver/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 #MEDIA_ROOT = os.path.join(BASE_DIR,'media_cdn')
 
 TEMP = os.path.join(BASE_DIR,'/temp')
