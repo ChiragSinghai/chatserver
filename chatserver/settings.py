f0233af418c6e3ff2644fde8458e2e16b32692b8
ChiragSinghai <jainchirag361@gmail.com>
2022-07-26 15:41:17 +0530

    .

--- chatserver/settings.py
+++ chatserver/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static'),
+    os.path.join(BASE_DIR,'assets'),
 ]
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
