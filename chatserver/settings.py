8408700c8d5be0cac4a9231c616c567e76f9713c
ChiragSinghai <jainchirag361@gmail.com>
2022-07-29 08:35:04 +0530

    .

--- chatserver/settings.py
+++ chatserver/settings.py
@@ -165,7 +165,7 @@ CHANNEL_LAYERS = {
     },
 }
 
-BASE_URL = "http://3.145.48.4"
+BASE_URL = "http://3.138.67.155"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760
 
