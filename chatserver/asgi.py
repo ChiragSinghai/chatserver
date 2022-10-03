8b52519e745d4b38ef99e56831b2707b6872d0a9
ChiragSinghai <jainchirag361@gmail.com>
2022-10-03 12:00:22 +0530

    Update asgi.py

--- chatserver/asgi.py
+++ chatserver/asgi.py
@@ -7,23 +7,42 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
+
+import os
+import django
+from decouple import config
+from channels.routing import get_default_application
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",  "chatserver.settings")
+django.setup()
+import notify.routing
+import public_chatroom.routing
+import privatechat.routing
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatserver.settings")
+application = ProtocolTypeRouter({
+  "http": get_asgi_application(),
+  "websocket": AuthMiddlewareStack(
+        URLRouter(
+            public_chatroom.routing.websocket_urlpatterns +
+            privatechat.routing.websocket_urlpatterns +
+            notify.routing.websocket_urlpatterns
+
+        )
+    ),
+})
+
+
+'''
 import os
 
 import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.handlers.asgi import ASGIHandler
-
-
-def get_asgi_application():
-    """
-    The public interface to Django's ASGI support. Return an ASGI 3 callable.
-
-    Avoids making django.core.handlers.ASGIHandler a public API, in case the
-    internal implementation changes or moves in the future.
-    """
-    django.setup(set_prefix=False)
-    return ASGIHandler()
 
 
 # from django.core.asgi import get_asgi_application
@@ -46,3 +65,4 @@ application = ProtocolTypeRouter({
         )
     ),
 })
+'''
