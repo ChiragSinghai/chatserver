92472f5fa7f3519786d5d5b6a49f65d5caee2930
ChiragSinghai <jainchirag361@gmail.com>
2022-10-19 23:18:26 +0530

    .

--- notify/routing.py
+++ notify/routing.py
@@ -1,4 +1,4 @@
-from django.urls import re_path
+from django.urls import path
 
 from . import consumers
-websocket_urlpatterns = [re_path(r'^notify/', consumers.NotifyConsumer.as_asgi())]
+websocket_urlpatterns = [path(r'^notify/', consumers.NotifyConsumer.as_asgi())]
