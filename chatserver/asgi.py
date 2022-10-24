325eb3565f50d6cacf79253d54429824ae3f1f4c
ChiragSinghai <jainchirag361@gmail.com>
2022-10-24 11:18:40 +0530

    Update asgi.py

--- chatserver/asgi.py
+++ chatserver/asgi.py
@@ -10,19 +10,39 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 import os
 import django
-from decouple import config
-from channels.routing import get_default_application
+#from decouple import config
+#from channels.routing import get_default_application
+from django.urls import re_path,path
+from . import consumers
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
+from channels.security.websocket import AllowedHostsOriginValidator
+#from django.core.asgi import get_asgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE",  "chatserver.settings")
 django.setup()
-import notify.routing
-import public_chatroom.routing
-import privatechat.routing
+from notify.consumers import NotifyConsumer
+from public_chatroom.consumers import ChatConsumer
+from privatechat.consumers import PrivateChatConsumer
+#import notify.routing
+#import public_chatroom.routing
+#import privatechat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatserver.settings")
+
+application = ProtocolTypeRouter({
+	'websocket': AllowedHostsOriginValidator(
+		AuthMiddlewareStack(
+			URLRouter([
+					re_path(r'/notify/', NotifyConsumer),
+                    re_path(r'^privatechat/(?P<room_id>\w+)/$', PrivateChatConsumer),
+					re_path(r'^public_chat/(?P<room_id>\w+)/$', ChatConsumer),
+			])
+		)
+	),
+})
+
+'''
 application = ProtocolTypeRouter({
   "websocket": AuthMiddlewareStack(
         URLRouter(
@@ -34,7 +54,7 @@ application = ProtocolTypeRouter({
     ),
 })
 
-
+'''
 '''
 import os
 
