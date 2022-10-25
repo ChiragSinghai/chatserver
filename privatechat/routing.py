19fbf8a89ffd3d46f1d675ea570353d31d1849b6
ChiragSinghai <jainchirag361@gmail.com>
2022-10-25 18:10:36 +0530

    .

--- privatechat/routing.py
+++ privatechat/routing.py
@@ -1,4 +1,4 @@
 from django.urls import re_path
-
+from django.conf.urls import url
 from . import consumers
-websocket_urlpatterns = [re_path(r'^privatechat/(?P<room_id>\w+)/$', consumers.PrivateChatConsumer.as_asgi())]
+websocket_urlpatterns = [url(r'^privatechat/(?P<room_id>\w+)/$', consumers.PrivateChatConsumer.as_asgi())]
--- public_chatroom/routing.py
+++ public_chatroom/routing.py
@@ -1,7 +1,7 @@
 from django.urls import re_path
-
+from django.conf.urls import url
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'^public_chat/(?P<room_id>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    url(r'^public_chat/(?P<room_id>\w+)/$', consumers.ChatConsumer.as_asgi()),
 ]
